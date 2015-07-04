9c1db0d110751bf530f84adf4f20584e198d92be
George Vardikos <George@Georges-MacBook-Pro.local>
2015-07-04 12:55:16 +0300

    Install devise, add a user, install devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :statuses
   resources :statuses
+
+  #devise asked to define the home page
+  root to: 'statuses#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
